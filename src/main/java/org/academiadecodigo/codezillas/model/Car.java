37fa765b22abe0330289e2542d8a15bf788925fc
João Ferreira <joaopsf09@gmail.com>
2019-07-25 19:08:51 +0000

    added car model

--- src/main/java/org/academiadecodigo/codezillas/model/Car.java
+++ src/main/java/org/academiadecodigo/codezillas/model/Car.java
@@ -2,6 +2,41 @@ package org.academiadecodigo.codezillas.model;
 
 public class Car {
 
-    
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private Integer horsePower;
 
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(Integer horsePower) {
+        this.horsePower = horsePower;
+    }
 }
