d59385261f62135c163a64919aa6f3e88cc9826b
João Ferreira <joaopsf09@gmail.com>
2019-07-25 19:15:44 +0000

    added suspension model

--- src/main/java/org/academiadecodigo/codezillas/model/components/Suspension.java
+++ src/main/java/org/academiadecodigo/codezillas/model/components/Suspension.java
@@ -1,4 +1,36 @@
 package org.academiadecodigo.codezillas.model.components;
 
 public class Suspension {
+
+    private Integer id;
+    private String brand;
+    private String type;
+
+
+    public Suspension() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
