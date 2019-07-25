61ca3d4ad0b9a8e034b11030b5ff64e514de4070
João Ferreira <joaopsf09@gmail.com>
2019-07-25 19:25:32 +0000

    added account model

---
+++ src/main/java/org/academiadecodigo/codezillas/model/Account.java
@@ -0,0 +1,35 @@
+package org.academiadecodigo.codezillas.model;
+
+public class Account {
+
+    private Integer id;
+    private String username;
+    private String password;
+
+    public Account() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
