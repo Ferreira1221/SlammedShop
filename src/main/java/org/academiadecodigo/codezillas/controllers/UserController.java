489964eed909f151b559955ec3585625b635645a
Goncalo Correia <thirtytech@outlook.com>
2019-08-07 23:17:10 +0000

    added homepage view

---
+++ src/main/java/org/academiadecodigo/codezillas/controllers/UserController.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.codezillas.controllers;
+
+import org.academiadecodigo.codezillas.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/main")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+    public String homepage() {
+        return "main/homepage";
+    }
+}
